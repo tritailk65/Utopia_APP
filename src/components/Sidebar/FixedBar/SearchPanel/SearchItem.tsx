e7cfb0215ced066f87cace18b6d8f9511fc1afb5
GiaHao <89515363+zetsubonn@users.noreply.github.com>
2023-10-30 22:21:21 +0700

    Search su dung service (loi)

--- src/components/Sidebar/FixedBar/SearchPanel/SearchItem.tsx
+++ src/components/Sidebar/FixedBar/SearchPanel/SearchItem.tsx
@@ -20,15 +20,84 @@
 
 // export default SearchItem;
 
+// import React, { useEffect, useState } from 'react';
+
+// type SearchItemProps = {
+//     user: {
+//         id: number;
+//     userName: string;
+//     createAt: Date;
+//     updateAt: Date;
+//     avatarPath: string;
+//     website: string;
+//     };
+// };
+
+// function SearchItem({ user }: SearchItemProps) {
+//     const handleClick = () => {
+//         alert(`User ID: ${user.id}, User name: ${user.userName}, Image Source: ${user.avatarPath}`);
+//     };
+
+//     const [avatarBase64, setAvatarBase64] = useState<string | null>(null);
+
+//     useEffect(() => {
+//         // Fetch the image blob data from the server
+//         fetch(`http://localhost:8080/api/User/Avatar/${user.id}`)
+//             .then((response) => response.blob())
+//             .then((blob) => {
+//                 blobToBase64(blob, (base64Data) => {
+//                     setAvatarBase64(base64Data);
+//                 });
+//             })
+//             .catch((error) => {
+//                 console.error(error);
+//             });
+//     }, [user.id]);
+
+//     function blobToBase64(blob: Blob, callback: (base64Data: string) => void) {
+//         const reader = new FileReader();
+//         reader.onload = function () {
+//             const dataUrl = reader.result as string;
+//             const base64 = dataUrl.split(',')[1];
+//             callback(base64);
+//         };
+//         reader.readAsDataURL(blob);
+//     }
+
+//     return (
+//         <div className="cursor-pointer hover:bg-gray-200 px-2 h-16 flex items-center mb-3" onClick={handleClick}>
+//             {avatarBase64 && (
+//                 <div className="mr-2">
+//                     <img src={`data:image/png;base64,${avatarBase64}`} alt="avt" className="w-14 h-14 rounded-full" />
+//                 </div>
+//             )}
+//             <div className="">
+//                 <h1 className="font-semibold text-lg">{user.userName}</h1>
+//                 <div className="text-gray-500 text-base">
+//                     <span className="mr-1">{user.id}</span>
+//                     <span className="mr-1">-</span>
+//                     <span className="">408k followers</span>
+//                 </div>
+//             </div>
+//         </div>
+//     );
+// }
+
+// export default SearchItem;
+
 import React, { useEffect, useState } from 'react';
 
+type User = {
+    id: number;
+    userName: string;
+    createAt: Date;
+    updateAt: Date;
+    avatarPath: string;
+    website: string;
+};
+
 type SearchItemProps = {
-    user: {
-        id: number;
-        userName: string;
-        avatarPath: string;
-        follower: string;
-    };
+    user: User;
 };
 
 function SearchItem({ user }: SearchItemProps) {
@@ -40,16 +109,10 @@ function SearchItem({ user }: SearchItemProps) {
 
     useEffect(() => {
         // Fetch the image blob data from the server
-        fetch(`http://localhost:8080/api/User/Avatar/${user.id}`)
-            .then((response) => response.blob())
-            .then((blob) => {
-                blobToBase64(blob, (base64Data) => {
-                    setAvatarBase64(base64Data);
-                });
-            })
-            .catch((error) => {
-                console.error(error);
-            });
+        if (user.avatarPath) {
+            // Sử dụng avatarPath để hiển thị hình ảnh trực tiếp
+            setAvatarBase64(user.avatarPath);
+        }
     }, [user.id]);
 
     function blobToBase64(blob: Blob, callback: (base64Data: string) => void) {
@@ -72,7 +135,7 @@ function SearchItem({ user }: SearchItemProps) {
             <div className="">
                 <h1 className="font-semibold text-lg">{user.userName}</h1>
                 <div className="text-gray-500 text-base">
-                    <span className="mr-1">{user.follower}</span>
+                    <span className="mr-1">{user.id}</span>
                     <span className="mr-1">-</span>
                     <span className="">408k followers</span>
                 </div>
--- src/components/Sidebar/FixedBar/SearchPanel/SearchPanel.tsx
+++ src/components/Sidebar/FixedBar/SearchPanel/SearchPanel.tsx
@@ -1,61 +1,107 @@
-// import SearchItem from './SearchItem';
-// import { useState } from 'react';
+
+// import { useState, useEffect } from 'react';
+// import SearchItem from './SearchItem'; 
 
 // function SearchPanel() {
 //     const [input, setInput] = useState<string>('');
+//     const [results, setResults] = useState<any[]>([]);
+
+//     useEffect(() => {
+//         const fetchData = () => {
+//             fetch('http://localhost:8080/api/User/')
+//                 .then((response) => {
+//                     if (!response.ok) {
+//                         throw new Error('Network response was not ok');
+//                     }
+//                     return response.json();
+//                 })
+//                 .then((json) => {
+//                     const filteredResults = json.Data.filter((user: any) => {
+//                         return user.userName.toLowerCase().includes(input.toLowerCase());
+//                     });
+//                     setResults(filteredResults);
+//                 })
+//                 .catch((error) => {
+//                     console.error('There has been a problem with your fetch operation:', error);
+//                     // Xử lý lỗi ở đây (ví dụ: hiển thị thông báo lỗi cho người dùng)
+//                 });
+//         };
+//         fetchData();
+//     }, [input]);
+
+//     const handleChange = (value: string) => {
+//         setInput(value);
+//     };
 
 //     return (
-//         <div>
-//             <h1 className="ml-2 text-3xl font-semibold mb-7 px-3">Search</h1>
-//             <div className="border-2 border-transparent rounded-lg mx-3 mb-7 bg-gray-200 group">
+//         <div className="p-3">
+//             <h1 className="text-3xl font-semibold mb-7">Search</h1>
+//             <div className="relative rounded-lg mb-7 bg-gray-200 group max-h-screen overflow-auto">
 //                 <input
 //                     className="bg-transparent w-full px-3 py-2 text-xl outline-none"
 //                     value={input}
-//                     onChange={(e) => setInput(e.target.value)}
+//                     onChange={(e) => handleChange(e.target.value)}
 //                 />
+//                 {input && (
+//                     <button
+//                         className="absolute top-2.5 right-3 text-gray-500 cursor-pointer"
+//                         onClick={() => setInput('')}
+//                     >
+//                         X
+//                     </button>
+//                 )}
 //             </div>
-//             {/* <div>
-//                 <SearchItem />
-//                 <SearchItem />
-//                 <SearchItem />
-//                 <SearchItem />
-//                 <SearchItem />
-//             </div> */}
+//             <div>
+//                 {results.map((user) => (
+//                     <SearchItem key={user.id} user={user} />
+//                 ))}
+//             </div>
 //         </div>
 //     );
 // }
 
 // export default SearchPanel;
-
+import { getListUser } from '../../../../services/user-service';
 import { useState, useEffect } from 'react';
-import SearchItem from './SearchItem'; // Đặt đường dẫn đúng đến SearchItem
+import SearchItem from './SearchItem'; 
+import { PostForViewer, UserPostForViewer } from '../../../../types/post-type';
+import { Response } from '../../../../types/api-type';
+import useDebounce from '../../../../hooks/useDebounce';
+
+interface User {
+    id: number;
+    userName: string;
+    createAt: Date;
+    updateAt: Date;
+    avatarPath: string;
+    website: string;
+}
 
 function SearchPanel() {
     const [input, setInput] = useState<string>('');
-    const [results, setResults] = useState<any[]>([]);
+    const [results, setResults] = useState<Response<UserPostForViewer[]> | undefined>();
+    const delayDebounce = useDebounce(input, 1000);
 
+    
     useEffect(() => {
-        const fetchData = () => {
-            fetch('http://localhost:8080/api/User/')
-                .then((response) => {
-                    if (!response.ok) {
-                        throw new Error('Network response was not ok');
-                    }
-                    return response.json();
-                })
-                .then((json) => {
-                    const filteredResults = json.Data.filter((user: any) => {
-                        return user.userName.toLowerCase().includes(input.toLowerCase());
-                    });
-                    setResults(filteredResults);
-                })
-                .catch((error) => {
-                    console.error('There has been a problem with your fetch operation:', error);
-                    // Xử lý lỗi ở đây (ví dụ: hiển thị thông báo lỗi cho người dùng)
-                });
+        const Data = async () => {
+            try {
+                const response: Response<UserPostForViewer[]> = await getListUser();
+                if (response && response.Status === 200) {
+                    
+                    setResults(response);
+                } else {
+                    throw new Error('Invalid response format');
+                }
+            } catch (error) {
+                console.error('There has been a problem with your fetch operation:', error);
+                // Xử lý lỗi ở đây (ví dụ: hiển thị thông báo lỗi cho người dùng)
+            }
         };
-        fetchData();
-    }, [input]);
+        
+
+        Data();
+    }, [delayDebounce]);
 
     const handleChange = (value: string) => {
         setInput(value);
@@ -80,10 +126,10 @@ function SearchPanel() {
                 )}
             </div>
             <div>
-                {results.map((user) => (
-                    <SearchItem key={user.id} user={user} />
-                ))}
-            </div>
+    {results && Array.isArray(results) && results.map((user: User) => (
+        <SearchItem key={user.id} user={user} />
+    ))}
+</div>
         </div>
     );
 }
