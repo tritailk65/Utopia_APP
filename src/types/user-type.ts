aa9b30fd063fe89ab0bd66759cb1aa127f869ed2
namdinhtuansmartlook <143388458+namdinhtuansmartlook@users.noreply.github.com>
2023-10-30 20:09:47 +0700

    merge

--- src/types/user-type.ts
+++ src/types/user-type.ts
@@ -1,11 +1,19 @@
 export type userInfo = {
-    id: number,
-    code: string,
-    slug: string,
-    username: string,
-    displayName : string
-    description: string,
-    createDate : Date,
-    updateDate : Date,
+    id: number;
+    code: string;
+    slug: string;
+    username: string;
+    displayName: string;
+    description: string;
+    createDate: Date;
+    updateDate: Date;
+};
 
+export interface UserPostForViewer {
+    id: number;
+    userName: string;
+    createAt: Date;
+    updateAt: Date;
+    avatarPath?: string | null;
+    website?: string | null;
 }
--- src/utils/api-utils.ts
+++ src/utils/api-utils.ts
@@ -2,4 +2,5 @@ export const backend_utils = {
     backend_url: 'http://localhost:8080/api',
     userController: '',
     postController: '/Post',
+    commentController: '/PostComment',
 };
