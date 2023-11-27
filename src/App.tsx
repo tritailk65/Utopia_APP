0b73e824741bf1678f99a38d3fb19520dbb78bb1
namdinhtuansmartlook <143388458+namdinhtuansmartlook@users.noreply.github.com>
2023-11-27 09:43:34 +0700

    SSE effect

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,63 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { PublicRoutes } from './routers/PublicRoutes';
 import { PrivateRoutes } from './routers/PrivateRoutes';
 import GlobalStyles from './styles/GlobalStyles';
-
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import './App.css';
 import FollowModal from './components/Modal/FollowModal/FollowModal';
 import CreatePostModal from './components/Modal/CreatePostModal/CreatePostModal';
 import PostCommentModal from './components/Modal/PostCommentModal/PostCommentModal';
 import Auth from './routers/ProtectRoutes';
 import EditPostModal from './components/Modal/EditPostModal/EditPostModal';
+import useGetUserInfo from './hooks/useGetUserInfo';
+import { useEffect } from 'react';
 
 function App() {
+    const user = useGetUserInfo();
+
+    const showToast = (message: string) => {
+        toast.info(message, {
+            position: 'bottom-right',
+            autoClose: 1000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+        });
+    };
+
+    useEffect(() => {
+        const connectSSE = () => {
+            try {
+                const eventSource = new EventSource(`http://localhost:8080/api/SSE/${user.id}/callNotification`);
+
+                eventSource.onmessage = (event) => {
+                    console.log('Received message from server:', event.data);
+                    if (event.data.includes('message')) showToast(event.data);
+                };
+
+                eventSource.onerror = (error) => {
+                    console.error('EventSource failed:', error);
+                    eventSource.close();
+                    reconnectSSE();
+                };
+
+                return () => {
+                    eventSource.close();
+                };
+            } catch (ex) {
+                console.log(ex);
+            }
+        };
+
+        const reconnectSSE = () => {
+            connectSSE();
+        };
+
+        if (user != null) {
+            connectSSE();
+        }
+    }, [user]);
+
     return (
         <Router>
             <GlobalStyles>
@@ -50,6 +98,7 @@ function App() {
                         })}
                     </Routes>
                 </div>
+                <ToastContainer />
                 <FollowModal />
                 <CreatePostModal />
                 <PostCommentModal />
